dc9f0b62a3c29aa95fc07e6b5c3703ad1278b830
BrunoSabadini <bruno_sabadini@hotmail.com>
2022-09-19 17:39:34 -0300

    TwoSum return inside if

--- TwoSum.swift
+++ TwoSum.swift
@@ -1,13 +1,13 @@
 //https://leetcode.com/problems/two-sum/
 
 func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-    var x:[Int] = []
-    for element in nums {
-        for i in (nums.firstIndex(of: element)!+1)..<nums.count {
-            if ((element + nums[i]) == target){
-                x = [i,nums.firstIndex(of: element)!]
+        for element in nums {
+            for i in (nums.firstIndex(of: element)!+1)..<nums.count {
+                if ((element + nums[i]) == target){
+                    print([i,nums.firstIndex(of: element)!])
+                    return [i,nums.firstIndex(of: element)!]
+                }
             }
         }
+        return []
     }
-    return x
-}
