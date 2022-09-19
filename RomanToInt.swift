0b140b2cdae7584af6553f10ee3ad17c76f7b766
BrunoSabadini <bruno_sabadini@hotmail.com>
2022-09-19 17:33:24 -0300

    Add TwoSum

--- RomanToInt.swift
+++ RomanToInt.swift
@@ -1,3 +1,5 @@
+//https://leetcode.com/problems/roman-to-integer/
+
 func romanToInt(_ s: String) -> Int {
   
   let romansDict:[String:Int] = ["I":1, "V":5,"X":10, "L":50,"C":100, "D":500,"M":1000]
